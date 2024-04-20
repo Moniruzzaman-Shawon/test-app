41e0f1d8e076edc3a04ec085c10350e21321ae51
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-04-21 03:19:30 +0600

    loop and map

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const laptop = ['Dell', 'Mac', 'Asus', 'Acer'];
+  const phones = ['iPhone', 'Samsung', 'Pixel'];
   return (
     <div className="App">
+      {/* loop */}
+
+      {/* {
+        laptop.map(lap => <li>Name: {lap}</li>)
+      } */}
+
       <header className="App-header">
         <Person name="Nokia" brand='seventh'></Person>
+        <Person name={laptop[0]} brand='one'></Person>
+        <Person name={laptop[2]} brand='ninth'></Person>
+        <Person name={laptop[1]} brand='three'></Person>
         <Person name='Samsung' brand='second'></Person>
         <Person name='Xiaomi' brand='fifth'></Person>
         <Friend phone='Apple'></Friend>
