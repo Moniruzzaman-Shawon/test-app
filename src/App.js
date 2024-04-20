62d16dd872197c754ed9e56ec654fe7090add7d0
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-04-21 02:17:07 +0600

    using props to dynamically add data

--- src/App.js
+++ src/App.js
@@ -5,21 +5,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name="Nokia" brand='seventh'></Person>
+        <Person name='Samsung' brand='second'></Person>
+        <Person name='Xiaomi' brand='fifth'></Person>
+        <Friend phone='Apple'></Friend>
+        <Friend phone='Dell'></Friend>
+        <Friend phone='HP'></Friend>
       </header>
     </div>
   );
 }
 
+/* dynamically added data using props */
+
+function Person(props) {
+  return (
+    <div className='person'>
+      <h1>{props.name}</h1>
+      <p>{props.brand}</p>
+    </div>
+  )
+}
+
+function Friend(props) {
+  console.log(props);
+  return (
+    <div className='friend'>
+      <h1>{props.phone}</h1>
+      <p>Opening Boom</p>
+    </div>
+  )
+}
 export default App;
